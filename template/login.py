2f0a564464997581d054fccc3f8c545f75b7751b
leonardo <leonardoalvriv@gmail.com>
2024-11-11 17:51:39 -0600

    remove streamlit menu

--- template/login.py
+++ template/login.py
@@ -1,44 +1,33 @@
-'''
 import streamlit as st
 
+from template.my_component import example
+
+'''
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+with open('template/credentials.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
 from my_component import example
 
-example.open()
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['pre-authorized']
+)
+
+
+st.session_state['authenticator']= authenticator
+
+name, authentication_status, username = authenticator.login('main')
+
+if authentication_status:
+    example.open(name, authenticator)
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
 '''
-import streamlit as st
-from streamlit_option_menu import option_menu
-from my_component.pages import Anotador, Entities, NER
-
-#def open(name, authenticator):
-def open():
-    with st.sidebar:
-        columns= st.columns((1,1))
-        with columns[0]:
-            if st.button("Clear states", use_container_width=True):
-                for key in st.session_state.keys():
-                    del st.session_state[key]
-        with columns[1]:
-            if st.button("Clear cache", use_container_width=True):
-                st.cache_data.clear()
-                
-
-        option= option_menu("Annotation Tool", ["Entities", "Annotate", "View Annotations", "NER", "File Logs", \
-                                                'Users', 'Statistics'], icons= ['gear', 'markdown', 'eye', \
-                                                                                'house-gear', 'filetype-csv', 'person', 'key'])
-        
-    if option == "Entities":
-        Entities.run()
-    elif option  == "Annotate":
-        Anotador.run()
-    elif option == "View Annotations":
-        NER.run()
-    elif option == "NER":
-        NER.run()
-    elif option == "File Logs":
-        NER.run()
-    elif option == 'Statistics':
-        NER.run()
-    elif option == 'Users':
-        NER.run()
-
-open()
+example.open()
