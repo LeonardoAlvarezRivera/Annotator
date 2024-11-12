1c762f1b2aa4a668debdf7824a3245e7dd231cf7
leonardo <leonardoalvriv@gmail.com>
2024-11-11 18:27:19 -0600

    remove streamlit menu

--- template/my_component/pages/Entities.py
+++ template/my_component/pages/Entities.py
@@ -33,8 +33,8 @@ page_anotador_default = """
 
 iframe[title="my_component.my_component"] {
     border: 0px;
-    width: 80vw;
-    height:95vh
+    width: 100vw;
+    height:100vh
 }
 
 .st-emotion-cache-1y4p8pa {
