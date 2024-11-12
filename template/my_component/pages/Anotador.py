cadb742e1daa2397349d8ddac6881104b7556faa
leonardo <leonardoalvriv@gmail.com>
2024-11-11 18:43:45 -0600

    remove streamlit menu

--- template/my_component/pages/Anotador.py
+++ template/my_component/pages/Anotador.py
@@ -33,7 +33,7 @@ page_anotador_default = """
 
 iframe[title="my_component.my_component"] {
     border: 0px;
-    width: calc(100vw - 300px );
+    width: calc(100vw - 500px );
     height: calc(100vh - 50px);
 }
 
