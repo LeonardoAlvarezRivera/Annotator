f649314496e31e07f86c65f7b4bfb5f28989c089
leonardo <leonardoalvriv@gmail.com>
2024-11-11 18:07:17 -0600

    remove streamlit menu

--- template/login.py
+++ template/login.py
@@ -1,31 +1,5 @@
 import streamlit as st
 
 from my_component import example
-"""
-import streamlit_authenticator as stauth
-import yaml
-from yaml.loader import SafeLoader
-with open('template/credentials.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
 
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['pre-authorized']
-)
-
-
-st.session_state['authenticator']= authenticator
-
-name, authentication_status, username = authenticator.login('main')
-
-if authentication_status:
-    example.open(name, authenticator)
-elif authentication_status == False:
-    st.error('Username/password is incorrect')
-elif authentication_status == None:
-    st.warning('Please enter your username and password')
-"""
 example.open()
